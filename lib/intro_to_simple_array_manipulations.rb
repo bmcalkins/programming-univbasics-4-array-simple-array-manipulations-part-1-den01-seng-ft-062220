d1a124d0b00d7f07de3bc8de4e1b07682cba027f
Bradley Calkins <b.raders1013@gmail.com>
2020-06-12 01:51:43 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end 
+
+dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+
+def pop_with_args(dog_breeds)
+  dog_breeds.pop()
+  small_dogs = pop_with_args(dog_breeds)
+  small_dogs.pop()
+  
+end
+
+def using_shift(array)  
+  array.shift
+end
+
